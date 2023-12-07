422259e2d50f5065c822e0ce258d6ed94e8c2338
Raafat Nagy <105138072+Raafat-Nagy@users.noreply.github.com>
2023-12-07 07:58:34 +0200

    Update Main.java

--- Task.04_LinkedList/LinkedList/src/Main.java
+++ Task.04_LinkedList/LinkedList/src/Main.java
@@ -1,77 +1,86 @@
 public class Main {
 
-    public static void main(String[] args) {
-        // Create a new linked list
-        LinkedList linkedList = new LinkedList();
+	public static void main(String[] args) {
+		// Create a new linked list
+		LinkedList linkedList = new LinkedList();
 
-        // Test addFirst and addLast
-        linkedList.addFirst(2);
-        linkedList.addFirst(1);
-        linkedList.addLast(3);
-        linkedList.addLast(4);
+		// Test addFirst and addLast
+		linkedList.addFirst(2);
+		linkedList.addFirst(1);
+		linkedList.addLast(3);
+		linkedList.addLast(4);
 
-        // Print the initial linked list
-        System.out.println("Initial Linked List:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
-        System.out.println("Is the linked list empty? " + linkedList.isEmpty());
+		// Print the initial linked list
+		System.out.println("Initial Linked List:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
+		System.out.println("Is the linked list empty? " + linkedList.isEmpty());
+		System.out.println();
 
-        // Test removeFirst and removeLast
-        linkedList.removeFirst();
-        linkedList.removeLast();
+		// Test the reverse function
+		linkedList.reverse();
 
-        // Print the linked list after removals
-        System.out.println("\nLinked List after removeFirst and removeLast:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
+		System.out.println("Reversed Linked List:");
+		linkedList.printLinkedList();
 
-        // Test clear
-        linkedList.clear();
-        System.out.println("\nLinked List after clear:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
-        System.out.println("Is the linked list empty? " + linkedList.isEmpty());
+		// Test removeFirst and removeLast
+		linkedList.removeFirst();
+		linkedList.removeLast();
 
-        // Test addFirst to an empty list
-        linkedList.addFirst(10);
+		// Print the linked list after removals
+		System.out.println("\nLinked List after removeFirst and removeLast:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
 
-        // Print the linked list after adding to an empty list
-        System.out.println("\nLinked List after adding to an empty list:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
-        System.out.println("Is the linked list empty? " + linkedList.isEmpty());
+		// Test clear
+		linkedList.clear();
+		System.out.println("\nLinked List after clear:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
+		System.out.println("Is the linked list empty? " + linkedList.isEmpty());
 
-        // Test set
-        linkedList.set(0, 20);
+		// Test addFirst to an empty list
+		linkedList.addFirst(10);
 
-        // Print the linked list after setting a value
-        System.out.println("\nLinked List after setting a value:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
+		// Print the linked list after adding to an empty list
+		System.out.println("\nLinked List after adding to an empty list:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
+		System.out.println("Is the linked list empty? " + linkedList.isEmpty());
 
-        // Test min and max
-        linkedList.addLast(15);
-        linkedList.addLast(5);
-        linkedList.addLast(25);
-        System.out.println("\nLinked List with additional elements:");
-        linkedList.printLinkedList();
-        System.out.println("Min: " + linkedList.min());
-        System.out.println("Max: " + linkedList.max());
+		// Test set
+		linkedList.set(0, 20);
 
-        // Test removeAt
-        linkedList.removeAt(1);
+		// Print the linked list after setting a value
+		System.out.println("\nLinked List after setting a value:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
 
-        // Print the linked list after removeAt
-        System.out.println("\nLinked List after removeAt:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
+		// Test min and max
+		linkedList.addLast(15);
+		linkedList.addLast(5);
+		linkedList.addLast(25);
+		System.out.println("\nLinked List with additional elements:");
+		linkedList.printLinkedList();
+		System.out.println("Min: " + linkedList.min());
+		System.out.println("Max: " + linkedList.max());
 
-        // Test remove by value
-        linkedList.remove(20);
+		// Test removeAt
+		linkedList.removeAt(1);
 
-        // Print the linked list after remove by value
-        System.out.println("\nLinked List after remove by value:");
-        linkedList.printLinkedList();
-        System.out.println("Size: " + linkedList.size());
-    }
+		// Print the linked list after removeAt
+		System.out.println("\nLinked List after removeAt:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
+
+		// Test remove by value
+		linkedList.remove(20);
+
+		// Print the linked list after remove by value
+		System.out.println("\nLinked List after remove by value:");
+		linkedList.printLinkedList();
+		System.out.println("Size: " + linkedList.size());
+
+
+	}
 }
